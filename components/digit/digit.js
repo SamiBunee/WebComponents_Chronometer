6947cb422f2fd31ea12b9f28d0a2a2ae675efdce
Samantha Andrade (SamiBunee) <samibuneely@gmail.com>
2024-06-27 01:18:35 +0200

    Added Digit WebComponent
    - Created Digit WebComponent class
    - Added Digit's constant js documents
    - Added Digit's styles js document

---
+++ components/digit/digit.js
@@ -0,0 +1,56 @@
+import style from './styles.js';
+import html from './template.js';
+import numbers from './numbers.js';
+
+class Digit extends HTMLElement {
+    constructor(){
+        super();
+        let shadowRoot = this.attachShadow({mode:'open'});
+        shadowRoot.innerHTML = `<style>${style}</style>${html}`;
+        this._digitContainer = this.shadowRoot.querySelector('#digit-container');
+    }
+
+    connectedCallback(){
+        this.number = 0;
+        setInterval(() => {
+            this.advance(this.number);
+        }, 100);
+    }
+
+    get number(){
+        return parseFloat(this.getAttribute('number'));
+    }
+
+    advance(number){
+        this.number = number === 9 ? 0: number + 1;
+    }
+
+    set number(number){
+        this.setAttribute('number', number);
+    }
+
+    static get observedAttributes(){
+        return ['number'];
+    }
+
+    attributeChangedCallback(name, oldNumber, newNumber){
+        switch(name) {
+            case 'number':
+                if (oldNumber != newNumber){
+                    this.renderNumber();
+                }
+        }
+    }
+
+    renderNumber(){
+        Array.from(this._digitContainer.children).forEach((child) => {
+            child.classList.add('white');
+        });
+
+        numbers.get(this.number).forEach((identifyer) => {
+            this._digitContainer.querySelector('#' + identifyer).classList.remove('white');
+        })
+    }
+}
+
+export default Digit;
---
+++ components/digit/template.js
@@ -0,0 +1,12 @@
+const html = `
+    <div id = "digit-container">
+        <div id="top-center" class = "border"></div>
+        <div id="top-left" class = "border"></div>
+        <div id="top-right" class = "border"></div>
+        <div id="middle" class = "border"></div>
+        <div id="down-center" class = "border"></div>
+        <div id="down-left" class = "border"></div>
+        <div id="down-right" class = "border"></div>
+    </div>
+`;
+export default html;
